d8e592fad714e88b7ba284e1699aab2a521df69d
Kyle <kdonnelly220@gmail.com>
2017-07-04 13:56:04 -0700

    Commiting half baked spinning enemy

--- Assets/Scripts/Attacks/Bullets/Beam.cs
+++ Assets/Scripts/Attacks/Bullets/Beam.cs
@@ -2,11 +2,25 @@
 
 public class Beam : MyMonoBehaviour
 {
-    private static readonly Vector3 HALF_SIZE = new Vector3(.2f,.2f,.1f);
+    private delegate void FixedUpdateAction(float deltaTime, float timeScale);
+
+    private static readonly float Y_SIZE = .1f;
     private static readonly float MAX_DISTANCE = 100f;
 
     public LayerMask layerMask;
+    public Range width;
+    public float widthExpandRate;
+    public float widthShrinkRate;
+    public float damage;
 
+    private FixedUpdateAction updateAction;
+    private float currentWidth;
+
+    //Figure out how to make this work.
+        //Also, Create a better hit animation. Where theres an explosion of lines from the hit point. The colors should be a little random, but mainly a specific color
+        //Crits are typically more deep red.
+        //Standard hits are oranges.
+        //Glances are yellowish.
     private LineRenderer lineRenderer;
     private Vector3[] linePositions;
 
@@ -14,20 +28,74 @@ public class Beam : MyMonoBehaviour
     {
         //TODO object pooling
         linePositions = new Vector3[2];
-        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer = GetComponentInChildren<LineRenderer>();
+        updateAction = DoNothingAction;
     }
 
     protected override void MyFixedUpdateWithDeltaTime(float myDeltaTime, float timeScale)
     {
-        //this needs to be tested, however it may be more fun to create the time bending thing
-        RaycastHit hitInfo;
-        if(!Physics.BoxCast(transform.position, HALF_SIZE, transform.forward, out hitInfo, transform.rotation, MAX_DISTANCE, layerMask, QueryTriggerInteraction.Ignore))
+        updateAction(myDeltaTime, timeScale);
+    }
+
+    public bool IsActive()
+    {
+        return enabled;
+    }
+
+    public void StartBeam()
+    {
+        gameObject.SetActive(true);
+        lineRenderer.startWidth = width.min;
+        lineRenderer.endWidth = width.min;
+    }
+
+    public void EndBeam()
+    {
+        updateAction = ShrinkBeamAction;
+    }
+
+    private void ExpandBeamAction(float deltaTime, float timeScale)
+    {
+        currentWidth += widthExpandRate * timeScale;
+        if(currentWidth >= width.max)
         {
-            Debug.Log("NOTHING WAS FOUND! There should always be something found");
+            currentWidth = width.max;
+            updateAction = FullAttackBeamAction;
+        }
+    }
+
+    private void ShrinkBeamAction(float deltaTime, float timeScale)
+    {
+        currentWidth += widthShrinkRate * timeScale;
+        if(currentWidth < width.min)
+        {
+            updateAction = DoNothingAction;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void DoNothingAction(float deltaTime, float timeScale) { }
+
+    private void FullAttackBeamAction(float deltaTime, float timeScale)
+    {
+        ShootBeam();
+    }
+
+    private void ShootBeam()
+    {
+        RaycastHit hitInfo;
+        Vector3 size = new Vector3(currentWidth / 2, Y_SIZE, currentWidth / 2);
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (!Physics.BoxCast(transform.position, size, transform.forward, out hitInfo, transform.rotation, MAX_DISTANCE, layerMask, QueryTriggerInteraction.Ignore))
+        {
             return;
         }
 
-        linePositions[0].Set(transform.position.x, transform.position.y, transform.position.z);
-        linePositions[1].Set(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
+        Hittable hittable = hitInfo.collider.GetComponent<Hittable>();
+        if(hittable)
+            hittable.Hit(damage);
+
+        lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y, transform.position.z));
+        lineRenderer.SetPosition(1, new Vector3(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z));
     }
 }
---
+++ Assets/Scripts/Movement/Spinner.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+
+public class Spinner : MyMonoBehaviour
+{
+    //TODO make this a range
+    public float maxSpinRate;
+    //TODO make this a range
+    public float spinUpRate;
+    //TODO make this a range
+    public float spinDownRate;
+    //TODO make this a range
+    public float waitTime;
+    //TODO make this a range?
+    public float beamStartEndRate;
+    public Beam[] beams;
+
+    private int direction;
+    private float spinSpeed;
+    private float spinChange;
+    private float waitTimer;
+
+    protected override void MyAwake()
+    {
+        spinSpeed = 0;
+        spinChange = 0;
+        direction = 1;
+        StartWait();
+    }
+
+    /*
+     * Start spinning 
+     */
+    public void StartSpinUp()
+    {
+        spinChange = spinUpRate;
+        direction = 1;
+    }
+
+    /*
+     * Stop spinning 
+     */
+    public void StartSpinDown()
+    {
+        spinChange = spinDownRate;
+    }
+
+    protected override void MyFixedUpdateWithDeltaTime(float myDeltaTime, float timeScale)
+    {
+        if(waitTimer > 0)
+        {
+            waitTimer -= myDeltaTime;
+            if(waitTimer < 0)
+                StartSpinUp();
+            return;
+        }
+        Debug.Log("STARTED SPINNING");
+
+        spinSpeed += spinChange * timeScale;
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        currentRotation.y += spinSpeed * direction * myDeltaTime;
+        transform.eulerAngles = currentRotation;
+
+        CheckSpinningBounds();
+        CheckBeamBounds();
+    }
+
+    private void CheckSpinningBounds()
+    {
+        if(spinSpeed > maxSpinRate)
+        {
+            spinSpeed = maxSpinRate;
+            spinChange = 0;
+        }
+        else if(spinSpeed < 0)
+        {
+            spinSpeed = 0;
+            spinChange = 0;
+            StartWait();
+        }
+    }
+
+    private void StartWait()
+    {
+        Debug.Log("STOP SPINNING");
+        waitTimer = waitTime;
+    }
+
+    private void CheckBeamBounds()
+    {
+        Test this shit.
+        if(!beams[0].IsActive() && spinSpeed > beamStartEndRate)
+        {
+            Debug.Log("Activate the beam!");
+            foreach(Beam beam in beams)
+            {
+                beam.StartBeam();
+            }
+        }
+        else if(beams[0].IsActive() && spinSpeed < beamStartEndRate)
+        {
+            Debug.Log("Beam is deactivating");
+            foreach(Beam beam in beams)
+            {
+                beam.EndBeam();
+            }
+        }
+    }
+}
